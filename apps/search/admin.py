9595bd80637dc79f27fed83a4ecc39707199969e
tushar-3549 <tushardiu3549@gmail.com>
2025-09-21 10:40:19 +0600

    search app fixed

--- apps/search/admin.py
+++ apps/search/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SearchSuggestion
 
-# Register your models here.
+@admin.register(SearchSuggestion)
+class SearchSuggestionAdmin(admin.ModelAdmin):
+    list_display = ("id", "entity_type", "display", "ref_id")
+    search_fields = ("display",)
+    list_filter = ("entity_type",)
--- apps/search/views.py
+++ apps/search/views.py
@@ -3,8 +3,18 @@ from rest_framework.filters import SearchFilter
 from .models import SearchSuggestion
 from .serializers import SearchSuggestionSerializer
 
+from rest_framework import filters
+
+# class SuggestionListView(ListAPIView):
+#     queryset = SearchSuggestion.objects.all()
+#     serializer_class = SearchSuggestionSerializer
+#     filter_backends = [SearchFilter]
+#     search_fields = ["display"]
+
+
 class SuggestionListView(ListAPIView):
     queryset = SearchSuggestion.objects.all()
     serializer_class = SearchSuggestionSerializer
-    filter_backends = [SearchFilter]
+    filter_backends = [SearchFilter, filters.OrderingFilter]
     search_fields = ["display"]
+    ordering_fields = ["entity_type", "display", "id"]
